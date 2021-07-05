9ac939bc591d9633a82ba70c4a3c9d9c3a53caf2
Nube Era <info@nubeera.com>
2021-07-05 18:58:54 +0530

    First Commit

---
+++ src/main/java/com/example/security/model/MyUser.java
@@ -0,0 +1,61 @@
+package com.example.security.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@Entity
+@ApiModel("This is User Login Model")
+public class MyUser {
+	@Id
+	@ApiModelProperty("This is user id property")
+	private int id;
+	
+	@ApiModelProperty("This is user name property")
+	@NotEmpty(message="Username is Mandatory")
+	private String name;
+	
+	@ApiModelProperty("This is user password property")
+	@NotEmpty(message="Password is Mandatory")
+	private String password;
+	
+	@ApiModelProperty("This is user activated property")
+	private boolean activated;
+	
+	@ApiModelProperty("This is user roles property")
+	private String roles;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isActivated() {
+		return activated;
+	}
+	public void setActivated(boolean activated) {
+		this.activated = activated;
+	}
+	public String getRoles() {
+		return roles;
+	}
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+}
